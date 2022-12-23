c851020e568ac41029d942dc3aa4bbe4bb5110a7
Pashalazarev <mymessagebox9@gmail.com>
2022-12-23 21:08:27 +0300

    1) Created UIPageViewController class 2) Implemented mandatory methods for flipping viewControllers with different colors

--- SpaceXRocketsApp/ViewController/MainPageViewController.swift
+++ SpaceXRocketsApp/ViewController/MainPageViewController.swift
@@ -10,69 +10,69 @@ import UIKit
 
 final class MainPageViewController: UIPageViewController {
     
-// MARK: - Private properties
-    
-    private var colourViewControllers: [UIViewController] = [
-        NetworkService.shared.redViewController,
-        NetworkService.shared.greenViewController,
-        NetworkService.shared.blueViewController
-    ]
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        dataSource = self
+    // MARK: - Private properties
         
-        if let firstViewController = colourViewControllers.first { 
-            setViewControllers([firstViewController],
-                               direction: .forward,
-                               animated: true,
-                               completion: nil)
+        private var colourViewControllers: [UIViewController] = [
+            NetworkService.shared.redViewController,
+            NetworkService.shared.greenViewController,
+            NetworkService.shared.blueViewController
+        ]
+        override func viewDidLoad() {
+            super.viewDidLoad()
+            dataSource = self
+            
+            if let firstViewController = colourViewControllers.first {
+                setViewControllers([firstViewController],
+                                   direction: .forward,
+                                   animated: true,
+                                   completion: nil)
+            }
         }
     }
-}
 
-// MARK: - UIPageViewControllerDataSource
+    // MARK: - UIPageViewControllerDataSource
 
-extension MainPageViewController: UIPageViewControllerDataSource {
-    
-    func pageViewController(_ pageViewController: UIPageViewController, viewControllerBefore viewController: UIViewController) -> UIViewController? {
-        guard let viewControllerIndex = colourViewControllers.firstIndex(of: viewController) else {
-            return nil
+    extension MainPageViewController: UIPageViewControllerDataSource {
+        
+        func pageViewController(_ pageViewController: UIPageViewController, viewControllerBefore viewController: UIViewController) -> UIViewController? {
+            guard let viewControllerIndex = colourViewControllers.firstIndex(of: viewController) else {
+                return nil
+            }
+            let previousIndex = viewControllerIndex - 1
+            guard previousIndex >= 0 else {
+                return nil
+            }
+            guard colourViewControllers.count > previousIndex else {
+                return nil
+            }
+            return colourViewControllers[previousIndex]
         }
-        let previousIndex = viewControllerIndex - 1
-        guard previousIndex >= 0 else {
-            return nil
+        
+        func pageViewController(_ pageViewController: UIPageViewController, viewControllerAfter viewController: UIViewController) -> UIViewController? {
+            guard let viewControllerIndex = colourViewControllers.firstIndex(of: viewController) else {
+                return nil
+            }
+            let nextIndex = viewControllerIndex + 1
+            let colourViewControllersCount = colourViewControllers.count
+            guard colourViewControllersCount != nextIndex else {
+                return nil
+            }
+            guard colourViewControllersCount > nextIndex else {
+                return nil
+            }
+            return colourViewControllers[nextIndex]
         }
-        guard colourViewControllers.count > previousIndex else {
-            return nil
+        
+    // MARK: - Methods for dots
+        func presentationCount(for pageViewController: UIPageViewController) -> Int {
+            colourViewControllers.count
         }
-        return colourViewControllers[previousIndex]
-    }
-    
-    func pageViewController(_ pageViewController: UIPageViewController, viewControllerAfter viewController: UIViewController) -> UIViewController? {
-        guard let viewControllerIndex = colourViewControllers.firstIndex(of: viewController) else {
-            return nil
+        func presentationIndex(for pageViewController: UIPageViewController) -> Int {
+            guard let firstViewController = viewControllers?.first,
+                  let firstViewControllerIndex = colourViewControllers.firstIndex(of: firstViewController) else {
+                      return 0
+                  }
+            return 0
         }
-        let nextIndex = viewControllerIndex + 1
-        let colourViewControllersCount = colourViewControllers.count
-        guard colourViewControllersCount != nextIndex else {
-            return nil
-        }
-        guard colourViewControllersCount > nextIndex else {
-            return nil
-        }
-        return colourViewControllers[nextIndex]
-    }
-    
-// MARK: - Methods for dots
-    func presentationCount(for pageViewController: UIPageViewController) -> Int {
-        colourViewControllers.count
-    }
-    func presentationIndex(for pageViewController: UIPageViewController) -> Int {
-        guard let firstViewController = viewControllers?.first,
-              let firstViewControllerIndex = colourViewControllers.firstIndex(of: firstViewController) else {
-                  return 0
-              }
-        return 0
-    }
 }
 
